d9433d15f35a611e24b32fb2798a57d9224a43d5
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-11-03 20:57:46 -0700

    step 3 done

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
 const {shuffleArray} = require('./utils')
 
+let array = [1, 2, 3]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('return an array the same length as the array passed in', () => {
+        expect(shuffleArray(array)).toHaveLength(3)
+})
+    test('return array in a different order', () => {
+        expect(shuffleArray(array)).not.toBe([1, 2, 3])
+    })
 })
